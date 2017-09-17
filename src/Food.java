71b31c5803bc24db2ed637b0682e66a7c85fdeb9
Marcin Paluchowski <victorarmy@gmail.com>
2017-09-17 11:16:48 +0200

    2nd version of Food. Constructor added.

--- src/Food.java
+++ src/Food.java
@@ -6,4 +6,12 @@ public class Food {
     double fat;
     double protein;
 
+    public Food(String name, String category, double calories, double carbohydrates, double fat, double protein) {
+        this.name = name;
+        this.category = category;
+        this.calories = calories;
+        this.carbohydrates = carbohydrates;
+        this.fat = fat;
+        this.protein = protein;
+    }
 }
--- src/FoodTest.java
+++ src/FoodTest.java
@@ -1,23 +1,10 @@
 public class FoodTest {
     public static void main(String[] args) {
-        Food tuna = new Food();
-        Food chicken = new Food();
 
-        tuna.name = "Tuna";
-        tuna.category = "Fish";
-        tuna.calories = 256;
-        tuna.fat = 15.7;
-        tuna.protein = 23.3;
-        tuna.carbohydrates = 4.0;
-
-        chicken.name = "Chicken";
-        chicken.category = "Meat";
-        chicken.calories = 128;
-        chicken.fat = 2.2;
-        chicken.protein = 21.4;
-        chicken.carbohydrates = 3.8;
+        Food tuna = new Food("Tuna", "Fish", 256, 4.0, 15.7, 23.3);
+        Food chicken = new Food("Chicken", "Meat", 128, 3.8, 2.2, 21.4);
 
         System.out.printf("Product name: %s\nProduct category: %s\nCalories: %.2f\n\tProtein: %.2f\n\tCarbohydrates: %.2f\n\tFat: %.2f\n\n", tuna.name, tuna.category, tuna.calories, tuna.protein, tuna.carbohydrates, tuna.fat);
-        System.out.printf("Product name: %s\nProduct category: %s\nCalories: %.2f\n\tProtein: %.2f\n\tCarbohydrates: %.2f\n\tFat: %.2f\n\n", chicken.name, chicken.category, chicken.calories, chicken.protein, chicken.carbohydrates, chicken.fat);
+        System.out.printf("Product name: %s\nProduct category: %s\nCalories: %.2f\n\tProtein: %.2f\n\tCarbohydrates: %.2f\n\tFat: %.2f\n", chicken.name, chicken.category, chicken.calories, chicken.protein, chicken.carbohydrates, chicken.fat);
     }
 }
